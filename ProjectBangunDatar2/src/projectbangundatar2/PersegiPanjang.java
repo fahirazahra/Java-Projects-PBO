3e11df52e0f686cce215d6c52581b4d4ba4f316c
Fahira Zahra <fahirazahra@student.uns.ac.id>
2021-10-11 23:44:40 +0700

    Create PersegiPanjang.java

---
+++ ProjectBangunDatar2/src/projectbangundatar2/PersegiPanjang.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author Fahira
+ */
+public class PersegiPanjang {
+    
+    int hitungLuas(int PP1, int PP2){
+        int luas = PP1 * PP2;
+        return luas;
+    }
+    double hitungLuas(double PP1, int PP2){
+        double luas = PP1 * PP2;
+        return luas;
+    }
+    double hitungLuas(int PP1, double PP2){
+        double luas = PP1 * PP2;
+        return luas;
+    }
+    double hitungLuas(double PP1, double PP2){
+        double luas = PP1 * PP2;
+        return luas;
+    }
+    
+    int hitungKeliling(int PP1, int PP2){
+        int keliling = (PP1 + PP2) * 2;
+        return keliling;
+    }
+    double hitungKeliling(double PP1, int PP2){
+        double keliling = (PP1 + PP2) * 2;
+        return keliling;
+    }
+    double hitungKeliling(int PP1, double PP2){
+        double keliling = (PP1 + PP2) * 2;
+        return keliling;
+    }
+    double hitungKeliling(double PP1, double PP2){
+        double keliling = (PP1 + PP2) * 2;
+        return keliling;
+    }
+}
