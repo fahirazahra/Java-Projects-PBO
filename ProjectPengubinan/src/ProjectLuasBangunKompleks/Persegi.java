e5a2a2950153e3b7870f015dfefd61a62b88b3a1
Fahira Zahra <fahirazahra@student.uns.ac.id>
2021-10-11 18:51:49 +0700

    Create Persegi.java

---
+++ ProjectPengubinan/src/ProjectLuasBangunKompleks/Persegi.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProjectLuasBangunKompleks;
+
+/**
+ *
+ * @author Fahira
+ */
+public class Persegi {
+    int sisi;
+    
+    int hitungLuas(){
+        int Luas = sisi * sisi;
+        return Luas;
+    }
+}
