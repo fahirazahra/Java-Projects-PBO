fe7ba438154a807ddee8f2ffd821578a0bd613da
Fahira Zahra <fahirazahra@student.uns.ac.id>
2021-10-11 23:44:06 +0700

    Create Operasi.java

---
+++ ProjectAdvancedOperasi/src/projectadvancedoperasi/Operasi.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectadvancedoperasi;
+
+/**
+ *
+ * @author Fahira
+ */
+public class Operasi {
+    
+    int jumlahkan(int a, int b){
+        return (a + b);
+    }
+    
+    int jumlahkan(int a, int b, int c){
+        return (a + b + c);
+    }
+    
+    double jumlahkan(double a, double b, double c){
+        return (a + b + c);
+    }
+    
+    // Project 1
+    double perkalian(int a, double b){
+        return(a * b);
+    }
+}
