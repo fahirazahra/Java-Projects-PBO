2334a046c9eb3abf67dd5f1161a78cf061b67885
Fahira Zahra <fahirazahra@student.uns.ac.id>
2021-10-11 23:44:38 +0700

    Create Persegi.java

---
+++ ProjectBangunDatar2/src/projectbangundatar2/Persegi.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author Fahira
+ */
+public class Persegi {
+    
+    int hitungLuas(int P1){
+        int luas = P1 * P1;
+        return luas;
+    }
+    double hitungLuas(double P1){
+        double luas = P1 * P1;
+        return luas;
+    }
+    
+    int hitungKeliling(int P1){
+        int keliling = P1 * 4;
+        return keliling;
+    }
+    double hitungKeliling(double P1){
+        double keliling = P1 * 4;
+        return keliling;
+    }
+}
