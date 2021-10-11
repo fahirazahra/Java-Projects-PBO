e695174d4b473061f2611ca30667878f107a825e
Fahira Zahra <fahirazahra@student.uns.ac.id>
2021-10-11 23:44:36 +0700

    Create Lingkaran.java

---
+++ ProjectBangunDatar2/src/projectbangundatar2/Lingkaran.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author Fahira
+ */
+public class Lingkaran {
+    
+    int hitungLuas(int L1){
+        int luas = L1 * L1;
+        return luas;
+    }
+    double hitungLuas(double L1){
+        double luas = L1 * L1;
+        return luas;
+    }
+    
+    int hitugKeliling(int L1){
+        int keliling = L1 * 2;
+        return keliling;
+    }
+    double hitungKeliling(double L1){
+        double keliling = 2 * L1;
+        return keliling;
+    }
+}
