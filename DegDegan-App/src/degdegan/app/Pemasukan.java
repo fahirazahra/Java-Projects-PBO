208d82acf30f31e3c07b7fe2e8bdd3099e9594fe
Fahira Zahra <fahirazahra@student.uns.ac.id>
2021-11-02 07:53:57 -0700

    Create Pemasukan.java

---
+++ DegDegan-App/src/degdegan/app/Pemasukan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package degdegan.app;
+
+/**
+ *
+ * @author Fahira
+ */
+
+public class Pemasukan {
+    // Untuk menentukan jumlah pemasukan dengan cara mengalikan jumlah buah dengan harga perbuah yang telah ditentukan
+    int pasokanHarga(int Jumlah){
+        int total = Jumlah * 8000;
+        return total;
+    }
+}
