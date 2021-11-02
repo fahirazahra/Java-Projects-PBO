6783b1eaa319d780ad37995e7925aecc6c675b8f
Fahira Zahra <fahirazahra@student.uns.ac.id>
2021-11-02 21:00:54 +0700

    Create PythagorasProject.java

---
+++ PythagorasProject/src/pythagorasproject/PythagorasProject.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pythagorasproject;
+
+/**
+ *
+ * @author Fahira
+ */
+public class PythagorasProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        OperasiPythagoras pythagoras = new OperasiPythagoras();
+        pythagoras.Menu();
+        pythagoras.OperasiPythagoras();
+    }   
+}
