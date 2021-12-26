65a1a10ad6a18b27b0dab398075bfe05ec7b633a
Fahira Zahra <fahirazahra@student.uns.ac.id>
2021-12-26 23:30:44 +0700

    Create SubMenu.java

---
+++ UAS_PendataanKaryawan/src/uas_pendataankaryawan/SubMenu.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas_pendataankaryawan;
+
+/**
+ *
+ * @author Fahira
+ */
+public abstract class SubMenu{
+    
+    abstract int SubMenu();
+    
+}
